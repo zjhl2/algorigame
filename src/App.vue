792506c50922567a255c271395048ebaecedde44
zjhl2 <478791469@qq.com>
2020-06-07 12:36:56 +0800

    1. add layer 2. modify graph

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,58 @@
 <template>
-  <div id="app">
-    <!-- <img alt="Vue logo" src="./assets/logo.png" /> -->
-    <!-- <HelloWorld msg="Welcome to Your Vue.js App" /> -->
-    <graph></graph>
-  </div>
+  <a-layout id="components-layout-demo-side" style="min-height: 100vh">
+    <a-layout-sider v-model="collapsed" collapsible>
+      <div id="logo"> Algorigame </div>
+      <a-menu theme="dark" :default-selected-keys="['1']" mode="inline">
+        <a-menu-item key="1">
+          <a-icon type="pie-chart" />
+          <span>Dijkstra</span>
+        </a-menu-item>
+        <a-menu-item key="2">
+          <a-icon type="desktop" />
+          <span>to be updated</span>
+        </a-menu-item>
+      </a-menu>
+    </a-layout-sider>
+    <a-layout>
+      <!-- <a-layout-header style="background: #fff; padding: 0" /> -->
+      <a-layout-content style="margin: 0 16px">
+        <graph></graph>
+      </a-layout-content>
+      <a-layout-footer style="text-align: center">
+        Author zjhl2
+      </a-layout-footer>
+    </a-layout>
+  </a-layout>
 </template>
-
 <script>
-import Graph from "./components/Graph.vue";
 
+import Graph from "./components/Graph"
 export default {
-  name: "App",
   components: {
     Graph
-  }
+  },
+  data() {
+    return {
+      collapsed: false,
+    };
+  },
 };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+* {
+  font-family: monospace;
+}
+
+#logo {   
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  color:aliceblue;
+  font-family: monospace;
+  font-size: xx-large;
+}
+#components-layout-demo-side .logo {
+  height: 32px;
+  background: rgba(255, 255, 255, 0.2);
+  margin: 16px;
 }
 </style>
--- src/static/VE.js
+++ src/static/VE.js
@@ -1,17 +1,18 @@
 export default {
     nodes: [
         { id: 0, x: 100, y: 100, r: 30 },
-        { id: 1, x: 400, y: 100, r: 30 },
-        { id: 2, x: 300, y: 200, r: 30 },
+        { id: 1, x: 800, y: 100, r: 30 },
+        { id: 2, x: 600, y: 200, r: 30 },
         { id: 3, x: 200, y: 300, r: 30 },
-        { id: 4, x: 300, y: 400, r: 30 },
+        { id: 4, x: 400, y: 400, r: 30 },
         { id: 5, x: 100, y: 400, r: 30 },
         { id: 6, x: 200, y: 500, r: 30 },
-        { id: 7, x: 400, y: 300, r: 30 },
-        { id: 8, x: 500, y: 500, r: 30 }
+        { id: 7, x: 900, y: 300, r: 30 },
+        { id: 8, x: 500, y: 500, r: 30 },
+        { id: 9, x: 1000, y: 500, r: 30 }
     ],
     edges: [
-        { from: 0, to: 1, length: 10},
+        { from: 0, to: 1, length: 20},
         { from: 1, to: 2, length: 10},
         { from: 0, to: 2, length: 10},
 
@@ -23,12 +24,17 @@ export default {
         { from: 4, to: 7, length: 9},
         { from: 4, to: 8, length: 10},
         { from: 6, to: 8, length: 5},
-        { from: 0, to: 3, length: 9},
+        { from: 0, to: 3, length: 4},
         { from: 5, to: 6, length: 10},
-        { from: 0, to: 5, length: 5},
+        { from: 0, to: 5, length: 8},
         { from: 2, to: 7, length: 9},
         { from: 5, to: 6, length: 10},
         { from: 1, to: 7, length: 3},
+        { from: 9, to: 7, length: 4},
+        { from: 4, to: 6, length: 10},
+        { from: 5, to: 4, length: 3},
+        { from: 7, to: 8, length: 10},
+        { from: 8, to: 9, length: 8}
 
     ]
   }
